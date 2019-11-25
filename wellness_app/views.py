58109fa79c2dd6f6b479edca11abde2ecc838713
CCowens93 <cassandracowens93@gmail.com>
2019-11-25 15:17:58 -0500

    added urls.py

--- wellness_app/views.py
+++ wellness_app/views.py
@@ -7,3 +7,19 @@ from .models import SleepGoal, ExerciseGoal, MentalHealthGoal, ExerciseSchedule,
 class SleepGoalView(viewsets.ModelViewSet):
     queryset = SleepGoal.objects.all()
     serializer_class = SleepGoalSerializer
+
+class ExerciseGoalView(viewsets.ModelViewSet):
+    queryset = ExerciseGoal.objects.all()
+    serializer_class = ExerciseGoalSerializer
+
+class MentalHealthGoalView(viewsets.ModelViewSet):
+    queryset = MentalHealthGoal.objects.all()
+    serializer_class = MentalHealthGoalSerializer
+
+class ExerciseScheduleView(viewsets.ModelViewSet):
+    queryset = ExerciseSchedule.objects.all()
+    serializer_class = ExerciseScheduleSerializer
+
+class DietaryGoalView(viewsets.ModelViewSet):
+    queryset = DietaryGoal.objects.all()
+    serializer_class = DietaryGoalSerializer
