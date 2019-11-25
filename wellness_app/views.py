c5e48984877b2cc1ff093d5169f36cc41b0a6312
CCowens93 <cassandracowens93@gmail.com>
2019-11-25 15:02:07 -0500

    beginning view.py

--- wellness_app/views.py
+++ wellness_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-
+from .serializers import SleepGoalSerializer, ExerciseGoalSerializer, MentalHealthGoalSerializer, ExerciseScheduleSerializer, DietaryGoalSerializer
 from .models import SleepGoal, ExerciseGoal, MentalHealthGoal, ExerciseSchedule, DietaryGoal
 
 # Create your views here.
+class SleepGoalView(viewsets.ModelViewSet):
+    queryset = SleepGoal.objects.all()
+    serializer_class = SleepGoalSerializer
