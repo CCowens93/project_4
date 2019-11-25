aab2318a93ec2a7b838d6e4287581a9c50a61690
CCowens93 <cassandracowens93@gmail.com>
2019-11-25 14:56:59 -0500

    finished serializers for now

--- wellness_app/serializers.py
+++ wellness_app/serializers.py
@@ -2,3 +2,28 @@ from rest_framework import serializers
 
 from .models import SleepGoal, ExerciseGoal, MentalHealthGoal, ExerciseSchedule, DietaryGoal
 
+class SleepGoalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SleepGoal
+        fields = ('id', 'sleep_goals', 'hours_of_sleep', 'bedtime', 'routine')
+
+class ExerciseGoalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExerciseGoal
+        fields = ('id', 'body_goals')
+
+class MentalHealthGoalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MentalHealthGoal
+        fields = ('id', 'mental_health_goal', 'medication', 'meditation', 'therapy')
+
+
+class ExerciseScheduleSerialixer(serializers.ModelSerializer):
+    class Meta:
+        model = ExerciseSchedule
+        field = ('id', 'workout_schedule', 'hours_of_exercise', 'daily_focus')
+
+class DietaryGoalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DietaryGoal
+        field = ('id', 'dietary_goal', 'elimination')
