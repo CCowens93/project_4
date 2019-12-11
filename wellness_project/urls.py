6874e507a1de0b960136baac223824182b248865
CCowens93 <cassandracowens93@gmail.com>
2019-12-11 16:06:16 -0500

    added a comma

--- wellness_project/urls.py
+++ wellness_project/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.FrontendAppView.as_view(),
-    path('api/', include('wellness_app.urls'))
-   
+    path('api/', include('wellness_app.urls')),
 ]
