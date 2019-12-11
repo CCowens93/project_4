025146c2f86cf01ec8b2e9ef62da122c11a2db88
CCowens93 <cassandracowens93@gmail.com>
2019-12-11 16:23:12 -0500

    bug fix

--- wellness_project/urls.py
+++ wellness_project/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.FrontendAppView.as_view(),
+    path('', views.FrontendAppView.as_view()),
     path('api/', include('wellness_app.urls')),
 ]
