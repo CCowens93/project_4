2710b4f963e29c1ca22d83f456132f2edf6af7bc
CCowens93 <cassandracowens93@gmail.com>
2019-12-11 15:58:55 -0500

    update

--- wellness_project/urls.py
+++ wellness_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.FrontendAppView.as_view(),
     path('api/', include('wellness_app.urls'))
    
 ]
